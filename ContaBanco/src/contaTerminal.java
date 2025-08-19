187af4582ba481ddb34db47aaff8a7f80d6138b2
willicastro <willybrs15@gmail.com>
2025-08-19 14:34:10 -0300

    entregue

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class contaTerminal {
+    
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("por favor digite o numero da conta ");
+        int numeroconta = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.println("por favor, digite o numero da agencia ");
+        String numeroagencia = scanner.nextLine();
+
+        System.out.println("digite o nome do cliente" );
+        String nomecliente = scanner.nextLine();
+
+        System.out.println("deposite um valor ");
+
+        double valorDeposito = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomecliente +" , obrigado por criar uma conta em nosso banco, sua agência é " 
+        +numeroagencia+ " conta " +numeroconta + " e seu saldo "+valorDeposito +" já está disponível para saque");
+
+
+
+
+        
     }
 }
